c032b061ac333f67808dff6d7c6b673ca09ef589
athanasiost <athanasios.topaltsis@bell.ca>
2024-05-09 12:10:23 -0400

    Initial commit

---
+++ RockPaperScissors/src/Moves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+
+    internal enum Moves
+    {
+        ROCK = 0,
+        PAPER = 1,
+        SCISSORS = 2
+    }
+}
