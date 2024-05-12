45424d4a054dde40b86595f3d50a1e980baad531
Athanasios Topaltsis <a.topaltsis@gmail.com>
2024-05-12 13:50:04 -0400

    Added comments

--- RockPaperScissors/Data/GameData.cs
+++ RockPaperScissors/Data/GameData.cs
@@ -3,6 +3,9 @@
     // Data class for local game data. (To be JSON deserialized and serialized)
     internal class GameData
     {
+        // Properties need to be public so that the Newtonsoft.Json library can access them.
+        // This is because the Newtonsoft library is actually it's own DLL file, and as we know, public access modifier exposes it to other assemblies.
+        // (You can see it's DLL file in the bin/Debug/net8.0 folder).
         public int TotalWins { get; set; }
         public int TotalGamesPlayed { get; set; }
         public int RockPlayedAmount { get; set; }
